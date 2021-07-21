76557b7b17dd732d8d33fdac3bf9079eb8f9b40e
thanakorn <thanakorn1101@hotmail.com>
2021-07-21 15:43:49 +0700

    edit

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -3,18 +3,12 @@ import Button from "@material-ui/core/Button";
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
 import "./Css/listView.scss";
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 
 
 function Detail () {
   
-  const [value, setValue] = React.useState(0);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  
+  
  
   
   
